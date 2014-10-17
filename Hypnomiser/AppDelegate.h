35025fef6e94ef317f2a769347fd7ec97a304b91
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-17 10:40:21 +0530

    Initial Commit

---
+++ Hypnomiser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnomiser
+//
+//  Created by Leonard Bogdonoff on 10/17/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
