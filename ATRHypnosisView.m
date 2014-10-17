2a581d526d79da5ab8d48a73ad5f27f80fe3f821
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-17 10:41:33 +0530

    1. Add hypnomiser view

---
+++ ATRHypnosisView.m
@@ -0,0 +1,21 @@
+//
+//  ATRHypnosisView.m
+//  Hypnomiser
+//
+//  Created by Leonard Bogdonoff on 10/17/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import "ATRHypnosisView.h"
+
+@implementation ATRHypnosisView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
